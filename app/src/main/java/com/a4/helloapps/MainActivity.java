fcc42b63033b1753195d5761120adfbe560ae372
RafiMlnf <rafimaulanaf03@gmail.com>
2023-10-30 19:24:25 +0700

    add file

---
+++ app/src/main/java/com/a4/helloapps/MainActivity.java
@@ -0,0 +1,36 @@
+package com.a4.helloapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+
+    private int secondCount = 1;
+
+    private  TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toast);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        int next = mCount;
+        mCount = secondCount;
+        secondCount = next + mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(next));
+    }
+}
